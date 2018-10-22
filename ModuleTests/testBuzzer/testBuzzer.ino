bef7721a779c269bb230c7170885dfacf32ea60b
borntoleave <rzlib2l@gmail.com>
2018-10-22 15:49:26 -0400

    added MIT liscense to existing codes

--- ModuleTests/testBuzzer/testBuzzer.ino
+++ ModuleTests/testBuzzer/testBuzzer.ino
@@ -1,36 +1,38 @@
 /* Customized note and melody player for OpenCat, and MEOW \(=^_^=)/
- * It also serves to test the baud rate of the serial moniter and the crystal frequency.
- * OpenCat happens to use 57600 baud rate (and a 20MHz crystal on Petoi's dedicated PCB).
- * If you cannot see this sentence on serial monitor, 
- *    there's something wrong with your Arduino IDE's configuration.
- *  
- * The beep() is better if played with a passive buzzer
- * The meow() can only be played with an active buzzer on PWM pins
- * On most Arduino, the PWM pins are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
- * If your buzzer cannot meow(), it's probably a passive buzzer.
- * 
- * Rongzhong Li
- * August 2017 
- * 
- * Copyright (c) 2018 Petoi LLC. 
+   It also serves to test the baud rate of the serial moniter and the crystal frequency.
+   OpenCat happens to use 57600 baud rate (and a 20MHz crystal on Petoi's dedicated PCB).
+   If you cannot see this sentence on serial monitor,
+      there's something wrong with your Arduino IDE's configuration.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+   The beep() is better if played with a passive buzzer
+   The meow() can only be played with an active buzzer on PWM pins
+   On most Arduino, the PWM pins are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
+   If your buzzer cannot meow(), it's probably a passive buzzer.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+   Rongzhong Li
+   August 2017
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+   Copyright (c) 2018 Petoi LLC.
+
+  The MIT License
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
 */
 
 #define BUZZER 5 // the PWM pin the ACTIVE buzzer is attached to
@@ -41,15 +43,15 @@ int loopCounter = 0;
 byte melody[] = {8, 13, 10, 13, 8,  0,  5,  8,  3,  5, 8,//tone
                  8, 8,  32, 32, 8, 32, 32, 32, 32, 32, 8 //relative duration, 8 means 1/8 note length
                 };
-                
+
 void beep(int note, float duration = 10, int pause = 0, byte repeat = 1 ) {
   if (note == 0) {
     analogWrite(BUZZER, 0);
     delay(duration);
     return;
   }
-  int freq = 220 * pow(1.059463, note-1); // 220 is note A3
-                                          //1.059463 comes from https://en.wikipedia.org/wiki/Twelfth_root_of_two
+  int freq = 220 * pow(1.059463, note - 1); // 220 is note A3
+  //1.059463 comes from https://en.wikipedia.org/wiki/Twelfth_root_of_two
   float period = 1000000.0 / freq / 2.0;
   for (byte r = 0; r < repeat; r++) {
     for (float t = 0; t < duration * 1000; t += period * 2) {
@@ -67,7 +69,7 @@ void playMelody(byte m[], int len) {
     beep(m[i], 1000 / m[len + i], 100);
 }
 
-void meow(int repeat = 1, int pause = 200,int startF = 150,  int endF = 255, int increment = 5) {
+void meow(int repeat = 1, int pause = 200, int startF = 150,  int endF = 255, int increment = 5) {
   for (int r = 0; r < repeat; r++) {
     for (int amp = startF; amp <= endF; amp += increment) {
       analogWrite(BUZZER, amp);
@@ -89,15 +91,15 @@ void setup()
 
 void loop() {
   if (loopCounter == 0) {
-    beep(1,500,500);
-    beep(13,500,500);
+    beep(1, 500, 500);
+    beep(13, 500, 500);
     playMelody(melody, sizeof(melody) / 2);
     Serial.println(loopCounter++);
     delay(1000);
   }
   else if (loopCounter < 2) {
-    meow(1,1000,100);
-    meow(2,100);
+    meow(1, 1000, 100);
+    meow(2, 100);
     Serial.println(loopCounter++);
   }
 }
--- TrivialTricks/updateMultipleElement/updateMultipleElement.ino
+++ TrivialTricks/updateMultipleElement/updateMultipleElement.ino
@@ -1,3 +1,28 @@
+/*
+  compare speed and efficiency of multiple ways to update array elements
+
+  Rongzhong Li
+  September 8, 2018
+
+  The MIT License
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
 #define PT(s) Serial.print(s)  //makes life easier
 #define PTL(s) Serial.println(s)
 void setup() {
@@ -13,7 +38,7 @@ void setup() {
   b = 10;
   for (byte i = 0; i < 3; i++)
     PTL(*a[i]);
-  byte c[] = {*ptr, *ptr, 3* *ptr};
+  byte c[] = {*ptr, *ptr, 3 * *ptr};
   PTL();
   for (byte i = 0; i < 3; i++)
     PTL(c[i]);
@@ -26,7 +51,7 @@ void setup() {
   PTL(b);
   for (byte i = 0; i < 3; i++)
     PTL(c[i]);
-   
+
 
 }
 
