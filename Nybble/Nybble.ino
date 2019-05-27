227fb34c324e0da78d7297ead45e1d885d8ad453
BigPapaZ <zrandhaw@fandm.edu>
2019-05-27 12:58:59 -0400

    Testing3

--- Nybble/Nybble.ino
+++ Nybble/Nybble.ino
@@ -543,7 +543,7 @@ void loop() {
             shutServos();
             break;
           }
-        case 'b': {break;}
+        case 'b': {PTLF("Stuff is gonna happen");break;}
         case 's': {
             PTLF("save calibration");
             saveCalib(servoCalibs);
